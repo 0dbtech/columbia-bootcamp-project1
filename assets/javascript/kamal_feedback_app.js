c65cc1c5a550834bb231d3cc7b3e06701eb34d3f
Kamal Mukherjee <kamalnyc17@gmail.com>
2019-03-01 07:45:10 -0500

    Project 01

--- assets/javascript/kamal_feedback_app.js
+++ assets/javascript/kamal_feedback_app.js
@@ -27,13 +27,11 @@ $(".form-control").on("click", function () {
 dataRef.ref().on("value", function (snapshot) {
     $("#feedback-table > tbody").empty();
     snapshot.forEach(function (childSnapshot) {
-
         var newRow = $("<tr>").append(
             $("<td>").text(childSnapshot.val().conName),
             $("<td>").text(childSnapshot.val().conRating),
             $("<td>").text(childSnapshot.val().conFeedback)
         );
-
         // Append the new row to the table
         $("#feedback-table > tbody").append(newRow);
     });
